a5afde5a67232602b1c1dd331131d00b442cff4d
SaraBarrett <sara.c.mon@gmail.com>
2026-02-09 12:19:10 +0000

    manipular

--- js/exs.js
+++ js/exs.js
@@ -1,8 +1,30 @@
-//ex 9
-function unicorn(){
-    let image = document.getElementById('unicorn');
-    let heading = document.getElementById('mainheading');
+//ex pp9
+function unicorn() {
+  let image = document.getElementById("unicorn");
+  let heading = document.getElementById("mainheading");
 
-    console.log(image);
-    console.log(heading);
+  console.log(image);
+  console.log(heading);
+}
+
+//ex pp12
+function todos() {
+  let doneTodos = document.querySelectorAll(".done");
+  console.log(doneTodos);
+
+  let checkbox = document.querySelector("input[type='checkbox']");
+  console.log(checkbox);
+}
+
+//pp15
+function changeTaste() {
+  //seleccionar
+  let mySpan = document.querySelector("span");
+  
+  if(mySpan.innerText == 'Yammi'){
+    mySpan.innerText = 'Yack'
+  }else{
+    mySpan.innerText = 'Yammi'
+  }
+
 }
--- js/script.js
+++ js/script.js
@@ -1,25 +1,54 @@
-function selectors(){
-    //alert('olá selectores!');
+function selectors() {
+  //alert('olá selectores!');
 
-    //selector pelo id
-    let myImage = document.getElementById('banner');
-    console.log(myImage);
+  //selector pelo id
+  let myImage = document.getElementById("banner");
+  console.log(myImage);
 
-    //selector pela tag
-    let h1 = document.getElementsByTagName('h1');
-    console.log(h1[0]);
+  //selector pela tag
+  let h1 = document.getElementsByTagName("h1");
+  console.log(h1[0]);
 
-    //seleccionar com o query selector
-    let myH1Wqueryselector = document.querySelector('h1');
-    console.log(myH1Wqueryselector);
+  //seleccionar com o query selector
+  let myH1Wqueryselector = document.querySelector("h1");
+  console.log(myH1Wqueryselector);
 
-    //selector pela class
-    let div = document.getElementsByClassName('toctitle');
-    console.log(div[0])
+  //selector pela class
+  let div = document.getElementsByClassName("toctitle");
+  console.log(div[0]);
 
-    //selector query selector com class
-    let myDivQuerySelector = document.querySelector('.toctitle');
-    console.log(myDivQuerySelector);
+  //selector query selector com class
+  let myDivQuerySelector = document.querySelector(".toctitle");
+  console.log(myDivQuerySelector);
+}
 
+function translateLang() {
+  //seleccionar
+  let myH1 = document.querySelector("h1");
 
+  //manipular o texto: propriedade innerText ou textContent
+  if (myH1.innerText == "Galinhas Sedosas") {
+    myH1.innerText = "Silkie Chickens";
+  } else {
+    myH1.innerText = "Galinhas Sedosas";
+  }
+}
+
+//manipular atributos
+function changeImage() {
+  //seleccionar imagem
+  let myImage = document.getElementById("banner");
+
+  //manipular o atributo src da imagem
+
+  if (
+    myImage.src ==
+    "https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"
+  ) {
+    myImage.src =
+      "https://elearning.cesae.pt/pluginfile.php/1/core_admin/logo/0x150/1674568226/Cesae.Full.png";
+  } else {
+    myImage.src =
+      "https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80";
+  }
 }
